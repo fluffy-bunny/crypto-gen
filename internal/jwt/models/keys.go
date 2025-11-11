89fa4da9f89876cad3bab57d34c29dfb56b89f0a
herb stahl <ghstahl@gmail.com>
2025-11-11 14:40:53 -0800

    fix

--- internal/jwt/models/keys.go
+++ internal/jwt/models/keys.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type (
-	 struct {
+	PublicJwk struct {
 		Alg string `json:"alg"`
 		Crv string `json:"crv,omitempty"` // For EC keys
 		Kid string `json:"kid"`
 		Kty string `json:"kty"`
 		Use string `json:"use"`
-		X PublicJwk  string `json:"x,omitempty"` // For EC keys
+		X   string `json:"x,omitempty"` // For EC keys
 		Y   string `json:"y,omitempty"` // For EC keys
 		E   string `json:"e,omitempty"` // For RSA keys
 		N   string `json:"n,omitempty"` // For RSA keys
 	}
+
 	PrivateJwk struct {
 		Alg string `json:"alg"`
 		Crv string `json:"crv,omitempty"` // For EC keys
